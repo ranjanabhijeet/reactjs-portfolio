57812a5fb795a904e0a8e798c794b5d6fef01bb5
Abhijeet Ranjan <ranjanabhijeet2004@gmail.com>
2023-12-05 20:33:35 +0530

    Change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
             Contact
           </p>
           <p className="text-gray-300 py-4 ">
-            Submit the form below or shoot me an email -
+            Submit the form below or shoot me an email-- -
             ranjanabhijeet2004@gmail.com
           </p>
         </div>
